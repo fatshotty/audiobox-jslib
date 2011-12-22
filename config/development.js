bb56a637bdb145980802a1e683b84ff016b0b12b
Fabio Tunno <fat@fatshotty.net>
2011-12-23 00:26:26 +0100

    init commit

---
+++ config/development.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  env: "development",
+
+  HTTPServer: "localhost",
+  HTTPPort: 5000,
+
+  UPServer: "localhost",
+  UPPort: 3000,
+
+  UserAgent: "AudioBox.fm 2 API framework"
+
+};
