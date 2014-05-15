920e3f4b29ababa8c69b5b8669d02855d77252bc
FatShotty <fat@fatshotty.net>
2014-05-15 18:33:34 +0200

    fix logger

--- index.js
+++ index.js
@@ -1,8 +1,71 @@
-function Logger() {
-  if (window.console) {
-    console.info.apply(console, arguments);
+var LEVEL, LEVELS, l;
+
+window.Logger = function() {
+  Logger.info.apply(Logger, arguments);
+};
+
+window.Logger.write = function(level, args) {
+  var arg, toString, _i, _len;
+  if (window.console && window.console[level]) {
+    if (window.console[level].apply) {
+      return window.console[level].apply(window.console, args);
+    } else {
+      toString = [];
+      for (_i = 0, _len = args.length; _i < _len; _i++) {
+        arg = args[_i];
+        toString.push("" + arg);
+      }
+      return window.console[level](toString.join(", "));
+    }
   }
-}
+};
+
+LEVELS = {
+  no: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  log: 4
+};
+
+LEVEL = LEVELS.no;
+
+Logger.level = function(l) {
+  if (l === void 0 || l === null) {
+    return LEVEL;
+  }
+  if (typeof l === "number") {
+    return LEVEL = l;
+  } else {
+    return LEVEL = LEVELS[l] || l;
+  }
+};
+Logger.error = function() {
+  if (LEVEL < LEVELS.error) {
+    return;
+  }
+  return Logger.write("error", arguments);
+};
+Logger.warn = function() {
+  if (LEVEL < LEVELS.warn) {
+    return;
+  }
+  return Logger.write("warn", arguments);
+};
+Logger.info = function() {
+  if (LEVEL < LEVELS.info) {
+    return;
+  }
+  return Logger.write("info", arguments);
+};
+Logger.log = function() {
+  if (LEVEL < LEVELS.log) {
+    return;
+  }
+  return Logger.write("log", arguments);
+};
+
+Logger.level( LEVELS.error );
 
 function ABX_IMPORTER(env, folder, cb){
   window.ABX = {};
