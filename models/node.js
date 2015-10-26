b5090c1a3542a08747a7ae8165bdf01539cce855
FatShotty <fat@fatshotty.net>
2015-10-26 21:52:07 +0100

    added login on node

--- models/node.js
+++ models/node.js
@@ -18,13 +18,12 @@ function Node(config, connectors){
 Node.prototype.__proto__ = Module.prototype;
 
 
-
-Node.__defineGetter__("END_POINT", function(value){
-  return Nodes.END_POINT;
-});
-
-Node.prototype.__defineGetter__("END_POINT", function(value){
-  return Node.END_POINT;
+Node.prototype.__defineGetter__("END_POINT", function(){
+  if ( this._parent instanceof Nodes ) {
+    return [this._parent.END_POINT, this.id].join( Connection.URISeparator );
+  } else {
+    return [Configuration.APIPath, "node"].join( Connection.URISeparator );
+  }
 });
 
 Node.DECLARED_FIELDS = Object.freeze({
@@ -54,6 +53,35 @@ Node.DECLARED_FIELDS = Object.freeze({
 
 
 
+Node.prototype.setAuthentication = function(token) {
+  var self = this;
+  var addAuthToken = function(request) {
+    if ( self._authentication_token ){
+      Logger("setting auth_token", self._authentication_token.substring(self._authentication_token.length - 7));
+      request.setAuthentication( "auth_token", "x-auth-token", self._authentication_token, true );
+    }
+  };
+
+  // Reset authentication
+
+  delete this._authentication_token;
+  this.RailsConnector.removeAllListeners("new_request");
+  this.NodeConnector.removeAllListeners("new_request");
+  this.DaemonConnector.removeAllListeners("new_request");
+
+  if ( token ) {
+    this._authentication_token = token;
+    // We have to set the first listener on the Connection class
+    // In this way we are sure Auth parameter is correctly set!
+    this.RailsConnector.on( "new_request", addAuthToken );
+    this.NodeConnector.on( "new_request",  addAuthToken );
+    this.DaemonConnector.on( "new_request",  addAuthToken );
+  }
+
+}
+
+
+
 Node.prototype.__defineSetter__("updated_at", function(value){
   if ( value ) {
     this.fields[ "updated_at" ] = new Date( value );
@@ -78,3 +106,52 @@ Node.prototype.__defineGetter__("events", function(){
   }
   return this._events;
 });
+
+Node.prototype._extractData = function(data){
+  return data.node || data.company_node;
+};
+
+
+Node.prototype.load = function(authToken){
+  // Reset all fields
+  var authToken = authToken || this._authentication_token;
+  this._clear();
+
+  if ( authToken ) {
+    this.setAuthentication(authToken);
+  }
+
+  var self = this;  // shortcut to 'this' instance
+
+  var request = this.RailsConnector.Request; // new request
+
+
+  // Set the 'extension' for the URL
+  request.requestFormat = this.Configuration.RequestFormats.JSON;
+
+
+  // Request successfully completed
+  request.success = function(res, data){
+
+    // data = JSON.parse(data);
+    var nodedata = self._extractData( data );
+    self._parseResponse( nodedata );
+    self.emit('login', true);
+
+  };
+
+  request.error = function(){
+    self._loaded = false;
+    self.emit('login', false);
+  }
+
+  return request.get( this.END_POINT );
+};
+
+
+Node.prototype._clear = function() {
+  Module.prototype._clear.call(this);
+  delete this._authentication_token
+  this._events && this._events._clear();
+  this._events = null;
+};
